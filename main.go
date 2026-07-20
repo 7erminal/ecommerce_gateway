7491958f1bb05bb3aa90fa72af0611861965bb3c
Terminal-Adawe <bede.abbe91@gmail.com>
2026-07-20 19:00:34 +0000

    changes

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	logs.SetLogger(logs.AdapterFile, `{"filename":"../logs/AMC-gateway.log"}`)
 
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:5174", "https://70b8-154-161-28-82.ngrok-free.app", "https://ecommerce.admin.readils.com"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:5174", "https://70b8-154-161-28-82.ngrok-free.app", "https://ecommerce.admin.readils.com", "https://admin.shopmallafrik.com"},
 		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "X-Requested-With", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
