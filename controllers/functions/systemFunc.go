29d0a5160d67acd007d514cc3bc743421087db37
Terminal-Adawe <bede.abbe91@gmail.com>
2026-09-24 17:08:41 +0000

    changes

--- controllers/functions/systemFunc.go
+++ controllers/functions/systemFunc.go
@@ -1913,7 +1913,7 @@ func RemoveApplicationShop(c *beego.Controller, v requests.ApplicationShopReques
 
 	request := api.NewRequest(
 		host,
-		"/v1/application/remove-shop",
+		"/v1/applications/remove-shop",
 		api.DELETE)
 	// request.Params = {"UserId": strconv.Itoa(int(userid))}
 	request.InterfaceParams["ShopId"] = v.ShopId
