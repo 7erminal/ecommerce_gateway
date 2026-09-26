eeb8224c4eb1e348b8c59bb6de29aa3af9615def
Terminal-Adawe <bede.abbe91@gmail.com>
2026-09-26 21:00:07 +0000

    changes

--- middlewares/authorizationMiddleware.go
+++ middlewares/authorizationMiddleware.go
@@ -98,7 +98,7 @@ func AuthWithRoleBasedAppMiddleware(ctx *context.Context) {
 		return
 	}
 
-	// logs.Info("User details are ", verifyToken.User)
+	logs.Info("User details are ", verifyToken.User)
 	ctx.Input.SetData("user", verifyToken.User)
 
 	// Check if user role is SUPER_ADMIN - if so, skip application verification
